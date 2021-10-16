9d4b0eb213f9a8e1011a2a38b23a9c2e4d5f0c5c
katarzyna.popieniuk <katarzyna.popieniuk@gmail.com>
2021-10-16 12:29:40 +0200

    init project

---
+++ src/main/java/com/example/demo/PjatkWayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjatkWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjatkWayApplication.class, args);
+	}
+
+}
